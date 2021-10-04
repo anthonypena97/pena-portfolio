2e513848f3c66bad655b1d9ca39a263bd9099e17
Anthony Pena <apena5@alumni.jh.edu>
2021-10-04 06:01:14 -0400

    achieved mobile responsive design

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -14,15 +14,17 @@ function About(props) {
     return (
       <section className="my-5 aboutStyle">
         <div className="aboutContainers aboutImage">
-          <img src={Image} className="my-2 " style={{ width: "100%" }} alt="cover" />
+          <img src={Image} className="my-2 " style={{ width: "90%" }} alt="cover" />
         </div>
         <div className=" aboutContainers aboutText">
           <p className="aboutTextTitle">
             Artist & Web Developer
           </p>
-          <p>
-            A graduate from the Peabody Music Conservatory at The Johns Hopkins University, and a current student at the University of Utah's Full Stack Web Development Professional Education program.
-          </p>
+          <div className="descriptionText">
+            <p>
+              A graduate from the Peabody Music Conservatory at The Johns Hopkins University, and a current student at the University of Utah's Full Stack Web Development Professional Education program.
+            </p>
+          </div>
         </div>
       </section>
     );
